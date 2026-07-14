29fff0ef988f4aa02f6d312b0d4ea82ad7c4f827
Leonard Schlenker <leonard.schlenker@rwth-aachen.de>
2026-07-14 12:57:37 +0200

    added availability check so that, before the recipes are sampled uniformly from the original dataset, we reduce the dataset to successfully scraped pages

--- scripts/merge_htmls_with_dataset.py
+++ scripts/merge_htmls_with_dataset.py
@@ -9,6 +9,13 @@ def merge_htmls_with_dataset():
     dataset = pl.scan_parquet(CLEANED_DATASET_PATH)
     htmls = pl.scan_parquet(HTML_PARQUET_PATH, low_memory=True)
 
+    # only leave the hash so that we can join and reduce `dataset` to 
+    # only the recipes that we already scraped
+    htmls = htmls.select(
+        pl.col("hash")
+    )
+
+    """
     dataset = htmls.join(dataset, 
                    on="hash", 
                    how="inner").select(
@@ -16,7 +23,44 @@ def merge_htmls_with_dataset():
                        pl.struct(role=pl.lit("user"),
                                  content=pl.col("html_data")).alias("input")# .struct.json_encode().alias("input")
                 ).sink_parquet(TRAINING_READY_DATASET_PATH, engine="streaming", compression="zstd")
+    """
 
+    dataset = htmls.join(
+        dataset, 
+        on='hash', 
+        how='inner'
+    )
+
+    # find the smallest group
+    domain_freqs = dataset.group_by(
+        pl.col("website")
+    ).len()
+
+    min_len = domain_freqs.select(pl.col("len")).min()
+
+    # sample uniformly from each website using `min_len`
+    website_partitions = dataset.collect().partition_by(
+        by="website", 
+        include_key=True
+    )
+
+    website_partitions = [web_df.sample(min_len) for web_df in website_partitions]
+
+    dataset = pl.concat(website_partitions, how='vertical')
+
+    htmls = pl.scan_parquet(HTML_PARQUET_PATH)
+
+    dataset = htmls.join(dataset, 
+                         on='hash', 
+                         how='inner'
+                        ).select(
+                             pl.col("response"), 
+                             pl.struct(role=pl.lit("user"), 
+                                       content=pl.col("html_data")).alias("input")
+                        ).sink_parquet(TRAINING_READY_DATASET_PATH, 
+                                       engine='streaming', 
+                                       compression='zstd'
+                        )
 
 if __name__ == '__main__': 
     merge_htmls_with_dataset()
